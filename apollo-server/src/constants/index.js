f8d4f8cd81adc1628f141b15a15d7ba2e732dbff
Valen Longfeather <valenlongfeather@Valens-MacBook-Pro.local>
2021-06-13 21:32:00 -0700

    update temperature

--- apollo-server/src/constants/index.js
+++ apollo-server/src/constants/index.js
@@ -5,7 +5,7 @@ const CONSTANTS = {
   // mongoDev: `mongodb://${process.env.MONGO_DATABASE_HOST}:27017/graphql-development`,
   mongoUri: `mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@${process.env.MONGO_DATABASE_HOST}:${process.env.MONGO_DATABASE_PORT}/${process.env.MONGO_INITDB_DATABASE}?authSource=admin`,
   lowestTemperatureThreshold: 62,
-  highestTemperatureThreshold: 72,
+  highestTemperatureThreshold: 75,
 };
 
 module.exports = { CONSTANTS };
